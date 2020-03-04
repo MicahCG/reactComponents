49e2b4fb7b055c8801701aa4a44a29ff180ee25d
MicahCG <46546797+MicahCG@users.noreply.github.com>
2020-03-03 22:16:53 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Heading";
+import List from "./List";
+
+ReactDOM.render(
+  <div>
+    <Heading />
+    <List />
+    <List />
+  </div>,
+  document.getElementById("root")
+);
